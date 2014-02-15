9242032dc73e6233866d27400aeffa34aabe9a59
wolfgang <wolfgang.deutsch@gmail.com>
2014-02-15 10:54:10 +0100

    Implemented Screen.clear

--- chip8_emulator/src/_main/Main.java
+++ chip8_emulator/src/_main/Main.java
@@ -8,16 +8,13 @@ package _main;
 import lib.Chip8Emulator;
 import lib.Screen;
 
-import java.awt.*;
-
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         DoubleBufferedWindow mainWindow = new DoubleBufferedWindow("Chip8 Emulator", 10, 10, 1024, 768);
-        Chip8Emulator emulator = new Chip8Emulator(new Screen());
+        Chip8Emulator emulator = new Chip8Emulator(new Screen(mainWindow));
         emulator.loadProgram(new int[] {123});
         while(emulator.isRunning())
         {
-            mainWindow.clear(Color.blue);
             emulator.tick();
             mainWindow.showBuffer();
             Thread.sleep(1);
--- chip8_emulator/src/lib/Screen.java
+++ chip8_emulator/src/lib/Screen.java
@@ -1,10 +1,23 @@
 package lib;
 
+import _main.DoubleBufferedWindow;
+
+import java.awt.*;
+
 /**
  * (c) 2013 Wolfgang Deutsch
  * Created on 2/14/14 at 8:27 PM
  */
 public class Screen {
+    private DoubleBufferedWindow window;
+
+    public Screen(DoubleBufferedWindow window)
+    {
+
+        this.window = window;
+    }
     public void clear()
-    {}
+    {
+        window.clear(Color.black);
+    }
 }
