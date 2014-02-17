97a1e6ee8175b3b7501fc9e0879dba551ca858f9
wolfgang <wolfgang.deutsch@gmail.com>
2014-02-17 16:52:06 +0100

    Added test for new version of Chip8Emulator

--- chip8_emulator/src/lib/MachineState.java
+++
@@ -1,9 +0,0 @@
-/**
- * (c) 2013 Wolfgang Deutsch
- * Created on 2/15/14 at 11:02 AM
- */
-
-package lib;
-
-public class MachineState {
-}
---
+++ chip8_emulator/src/unittests/Chip8EmulatorTest.java
@@ -0,0 +1,42 @@
+/**
+ * (c) 2013 Wolfgang Deutsch
+ * Created on 2/17/14 at 4:46 PM
+ */
+
+package unittests;
+
+import lib.Chip8Emulator;
+import lib.Chip8OpCode;
+import lib.OpCodeDecoder;
+import lib.Screen;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InOrder;
+
+import static org.mockito.Mockito.*;
+
+public class Chip8EmulatorTest {
+
+    private Screen screen;
+    private OpCodeDecoder opCodeDecoder;
+    private Chip8OpCode opCode;
+    private Chip8Emulator emulator;
+    private InOrder order;
+
+    @Before
+    public void setUp() throws Exception {
+        screen = mock(Screen.class);
+        opCodeDecoder = mock(OpCodeDecoder.class);
+        opCode = mock(Chip8OpCode.class);
+        emulator = new Chip8Emulator(screen, opCodeDecoder);
+        order = inOrder(opCode, screen);
+    }
+
+    @Test
+    public void tick_processNextOpcodeAndDrawScreen() throws Exception {
+        when(opCodeDecoder.getNext()).thenReturn(opCode);
+        emulator.tick();
+        order.verify(opCode).process();
+        order.verify(screen).draw();
+    }
+}
