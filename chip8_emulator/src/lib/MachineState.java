107fa1e92b50f67f893d7e48d37b7be07aff43d3
wolfgang <wolfgang.deutsch@gmail.com>
2014-02-21 16:42:26 +0100

    Added a stack to MachineState

--- chip8_emulator/src/lib/MachineState.java
+++ chip8_emulator/src/lib/MachineState.java
@@ -5,8 +5,28 @@
 
 package lib;
 
+import java.util.EmptyStackException;
+import java.util.Stack;
+
 public class MachineState {
+    private static final int MaxStackSize = 64;
     public int pc = 0x200;
     public int[] V = new int[16];
     public int I = 0;
+    private Stack<Integer> stack = new Stack<>();
+
+    public void pushOntoStack(int value)
+    {
+        if (stack.size() == MaxStackSize)
+            throw new StackOverflowError();
+        stack.push(value);
+    }
+
+    public int popFromStack()
+    {
+        if (!stack.empty())
+            return stack.pop();
+        else
+            throw new EmptyStackException();
+    }
 }
---
+++ chip8_emulator/src/unittests/MachineStateStackTest.java
@@ -0,0 +1,51 @@
+/**
+ * (c) 2013 Wolfgang Deutsch
+ * Created on 2/21/14 at 4:32 PM
+ */
+
+package unittests;
+
+import lib.MachineState;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.EmptyStackException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.fail;
+
+public class MachineStateStackTest {
+
+    private MachineState machineState;
+
+    @Before
+    public void setUp() throws Exception {
+        machineState = new MachineState();
+    }
+
+    @Test
+    public void pushThenPop() throws Exception {
+        machineState.pushOntoStack(15);
+        assertThat(machineState.popFromStack(), is(15));
+    }
+
+    @Test(expected = EmptyStackException.class)
+    public void popFromEmptyStackThrowsError() throws Exception {
+        machineState.popFromStack();
+    }
+
+    @Test
+    public void pushingValuesBeyond64ThrowsError() throws Exception {
+        for (int i = 0; i<64; ++i)
+            machineState.pushOntoStack(i);
+
+        try {
+            machineState.pushOntoStack(100);
+            fail("should have thrown StackOverflowError");
+        } catch (StackOverflowError e)
+        {
+
+        }
+    }
+}
