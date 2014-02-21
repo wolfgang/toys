a151a709bcba23773e6cdbd331a84c7cf06f20a2
wolfgang <wolfgang.deutsch@gmail.com>
2014-02-21 10:14:25 +0100

    Cleaned up op code keys

--- chip8_emulator/src/lib/OpCodeFactory.java
+++ chip8_emulator/src/lib/OpCodeFactory.java
@@ -14,26 +14,26 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class OpCodeFactory {
 
-    private static final Integer _00E0 = 0;
-    private static final Integer DXYN = 1;
-    private static final Integer _1NNN = 2;
+    private static final Integer OP_00E0 = 0;
+    private static final Integer OP_DXYN = 1;
+    private static final Integer OP_1NNN = 2;
 
     ConcurrentHashMap<Integer, OpCode> opCodesById = new ConcurrentHashMap<>();
     public OpCodeFactory() {
-        opCodesById.put(_00E0, new OpCode00E0());
-        opCodesById.put(DXYN, new OpCodeDXYN());
-        opCodesById.put(_1NNN, new OpCode1NNN());
+        opCodesById.put(OP_00E0, new OpCode00E0());
+        opCodesById.put(OP_DXYN, new OpCodeDXYN());
+        opCodesById.put(OP_1NNN, new OpCode1NNN());
     }
 
     public OpCode getOpCode(int code) throws InvalidOpCode {
         if ((code & 0xD000) == 0xD000)
-            return opCodesById.get(DXYN);
+            return opCodesById.get(OP_DXYN);
 
         if ((code & 0x1000) == 0x1000)
-            return opCodesById.get(_1NNN);
+            return opCodesById.get(OP_1NNN);
 
         if (code == 0x00E0)
-            return opCodesById.get(_00E0);
+            return opCodesById.get(OP_00E0);
 
         throw new InvalidOpCode(code);
     }
