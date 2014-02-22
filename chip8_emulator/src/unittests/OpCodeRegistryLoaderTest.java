646ab88715158894daa4eccf024557cb2681926e
wolfgang <wolfgang.deutsch@gmail.com>
2014-02-22 11:45:31 +0100

    added a test for registering all known op codes

--- chip8_emulator/src/unittests/OpCodeRegistryLoaderTest.java
+++ chip8_emulator/src/unittests/OpCodeRegistryLoaderTest.java
@@ -5,27 +5,65 @@
 
 package unittests;
 
-import lib.OpCodeRegistry;
-import lib.OpCodeRegistryLoader;
+import lib.*;
+import lib.OpCodes.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
 
-
-@SuppressWarnings("ALL")
 public class OpCodeRegistryLoaderTest {
 
     private OpCodeRegistry opCodeRegistry;
-    private OpCodeRegistryLoader loader;
+    private Display display;
+    private Memory memory;
 
     @Before
     public void setUp() throws Exception {
-        opCodeRegistry = mock(OpCodeRegistry.class);
-        loader = new OpCodeRegistryLoader(opCodeRegistry, null, null);
+        opCodeRegistry = new OpCodeRegistry(new OpCodeResolver());
+        display = mock(Display.class);
+        memory = mock(Memory.class);
+        OpCodeRegistryLoader loader = new OpCodeRegistryLoader(opCodeRegistry, memory, display);
+        loader.registerKnownOpCodes();
     }
 
     @Test
-    public void registerKnownOPpCodes() throws Exception {
+    public void registersOpCode00E0() throws Exception {
+        OpCode00E0 opCode = registersOpCode(0x00E0, OpCode00E0.class);
+        assertThat(opCode.getDisplay(), is(display));
+    }
+
+    @Test
+    public void registersOpCodeDXYN() throws Exception {
+        OpCodeDXYN opCode = registersOpCode(0xD000, OpCodeDXYN.class);
+        assertThat(opCode.getDisplay(), is(display));
+        assertThat(opCode.getMemory(), is(memory));
+    }
+
+    @Test
+    public void registersSimpleOpCodes() throws Exception {
+        registersOpCode(0x00EE, OpCode00EE.class);
+        registersOpCode(0x1000, OpCode1NNN.class);
+        registersOpCode(0x2000, OpCode2NNN.class);
+        registersOpCode(0x3000, OpCode3XNN.class);
+        registersOpCode(0x4000, OpCode4XNN.class);
+        registersOpCode(0x5000, OpCode5XY0.class);
+        registersOpCode(0x6000, OpCode6XNN.class);
+        registersOpCode(0x7000, OpCode7XNN.class);
+        registersOpCode(0x8000, OpCode8XY0.class);
+        registersOpCode(0x8001, OpCode8XY1.class);
+        registersOpCode(0x8002, OpCode8XY2.class);
+        registersOpCode(0x8003, OpCode8XY3.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T extends OpCode> T registersOpCode(int code, Class<T> cls) throws InvalidOpCode {
+        OpCode opCode = opCodeRegistry.getOpCode(code);
+        assertThat(opCode, instanceOf(cls));
+        return (T) opCode;
     }
 }
+
