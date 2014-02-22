ff176021f3c33366ccd3337c444c2bd82ae8a68d
wolfgang <wolfgang.deutsch@gmail.com>
2014-02-22 14:37:47 +0100

    Simplified opcode resolving

--- chip8_emulator/src/lib/OpCodeResolver.java
+++ chip8_emulator/src/lib/OpCodeResolver.java
@@ -6,69 +6,35 @@
 package lib;
 
 public class OpCodeResolver {
+    private int[] masks = {
+            0xD000,
+            0x800E,
+            0x8007,
+            0x8006,
+            0x8005,
+            0x8004,
+            0x8003,
+            0x8002,
+            0x8001,
+            0x8000,
+            0x7000,
+            0x6000,
+            0x5000,
+            0x4000,
+            0x3000,
+            0x2000,
+            0x1000,
+            0x00EE,
+            0x00E0,
+    };
+
 
     public int getOpCodeId(int code) {
-        if (isMask(code, 0xD000))
-            return 0xD000;
-
-        if (isMask(code, 0x800E))
-            return 0x800E;
-
-        if (isMask(code, 0x8007))
-            return 0x8007;
-
-        if (isMask(code, 0x8006))
-            return 0x8006;
-
-        if (isMask(code, 0x8005))
-            return 0x8005;
-
-        if (isMask(code, 0x8004))
-            return 0x8004;
-
-        if (isMask(code, 0x8003))
-            return 0x8003;
-
-        if (isMask(code, 0x8002))
-            return 0x8002;
-
-        if (isMask(code, 0x8001))
-            return 0x8001;
-
-        if (isMask(code, 0x8000))
-            return 0x8000;
-
-        if (isMask(code, 0x7000))
-            return 0x7000;
-
-        if (isMask(code, 0x6000))
-            return 0x6000;
-
-        if (isMask(code, 0x5000))
-            return 0x5000;
-
-        if (isMask(code, 0x4000))
-            return 0x4000;
-
-        if (isMask(code, 0x3000))
-            return 0x3000;
-
-        if (isMask(code, 0x2000))
-            return 0x2000;
-
-        if (isMask(code, 0x1000))
-            return 0x1000;
-
-        if (code == 0x00E0)
-            return 0x00E0;
-
-        if (code == 0x00EE)
-            return 0x00EE;
-
+        for (int mask : masks) {
+            if ((code & mask) == mask)
+                return mask;
+        }
         return -1;
     }
 
-    private boolean isMask(int code, int mask) {
-        return (code & mask) == mask;
-    }
 }
