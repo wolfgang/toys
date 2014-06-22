6059f081d3bb778ba9282fa14a73866a0af83052
wolfgang <wolfgang.deutsch@gmail.com>
2014-06-22 19:04:51 +0200

    remove debug output

--- elixir/ttt/lib/ai.ex
+++ elixir/ttt/lib/ai.ex
@@ -2,13 +2,17 @@ defmodule Ai do
 	def get_move board, move_gen_fn \\ &MoveEval.get_moves/1, random_fn \\&:random.uniform/1 do
     possible_moves = move_gen_fn.(board)
 
-		lst = Enum.map(possible_moves,
-									 fn({x,y}) -> "#{Integer.to_string(x)}/#{Integer.to_string(y)}" end)
+		#output_moves possible_moves
 
-		Enum.map(lst, fn(s) -> IO.write "#{s} " end)
-		IO.puts ""
-		
     :lists.nth(random_fn.(length(possible_moves)), possible_moves)
-		
 	end
+
+	defp output_moves moves do
+    lst = Enum.map(moves,
+                   fn({x,y}) -> "#{Integer.to_string(x)}/#{Integer.to_string(y)}" end)
+
+    Enum.map(lst, fn(s) -> IO.write "#{s} " end)
+    IO.puts ""
+	end
+
 end
