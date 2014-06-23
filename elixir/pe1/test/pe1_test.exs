6e51d5c7390afaeba401c530036b089ca117d044
wolfgang <wolfgang.deutsch@gmail.com>
2014-06-23 20:26:33 +0200

    formatting fix

--- elixir/pe1/test/pe1_test.exs
+++ elixir/pe1/test/pe1_test.exs
@@ -13,7 +13,7 @@ defmodule Pe1Test do
 
 	test "sum" do
 		assert Pe1.sum(10) == 23
-    assert Pe1.sum(16) == Enum.sum [3, 6, 9, 12, 5, 10, 15]
+		assert Pe1.sum(16) == Enum.sum [3, 6, 9, 12, 5, 10, 15]
 		assert Pe1.sum(21) == Enum.sum [3, 6, 9, 12, 18, 5, 10, 15, 20]
 		assert Pe1.sum(1000) == 233168
 	end
