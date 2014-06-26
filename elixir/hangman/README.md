4a06b7dfaa3cfdfe59b00c744cfbcccff04addbf
wolfgang <wolfgang.deutsch@gmail.com>
2014-06-26 07:46:51 +0200

    initial commit with first test

---
+++ elixir/hangman/README.md
@@ -0,0 +1,4 @@
+Hangman
+=======
+
+** TODO: Add description **
---
+++ elixir/hangman/test/hangman_test.exs
@@ -0,0 +1,30 @@
+defmodule HangmanTest do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  test "print initial screen to the console, with N dashes for a word with length N" do
+    output = capture_io(fn -> print_initial_screen("1234") end)
+    assert String.split(output, "\n", trim: true) ===
+      [
+       "=====|    Word:    ----",
+       "|    |    Guess:",   
+       "     |    Misses:",
+       "     |",
+       "     |",
+       "     |",
+       "========"
+    ]
+  end
+
+  def print_initial_screen word do
+    IO.puts "=====|    Word:    ----"
+    IO.puts "|    |    Guess:"
+    IO.puts "     |    Misses:"
+    IO.puts "     |"
+    IO.puts "     |"
+    IO.puts "     |"
+    IO.puts "========"
+
+  end
+  
+end
